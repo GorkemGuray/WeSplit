1536d5a855ddcd5222846c32bc0fe96527d643bd
Görkem Güray <grkmgry@gmail.com>
2024-01-14 21:22:35 +0300

    Day 17 #Hiding the keyboard

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,7 +11,19 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amaountIsFocused: Bool
+    
     let tipPercentages = [10, 15, 20, 25, 0]
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
     
     var body: some View {
         NavigationStack {
@@ -21,6 +33,7 @@ struct ContentView: View {
                               value: $checkAmount,
                               format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                     .keyboardType(.decimalPad)
+                    .focused($amaountIsFocused)
                     
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2..<100) {
@@ -38,8 +51,19 @@ struct ContentView: View {
                     }
                     .pickerStyle(.segmented)
                 }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
             }
             .navigationTitle("WeSplit")
+            .toolbar {
+                if amaountIsFocused {
+                    Button("Done") {
+                        amaountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
